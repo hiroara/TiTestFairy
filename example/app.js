d9bb16028b2e53349acda5713166dee2831ac8cb
Arai Hiroki <hiroara62@gmail.com>
2015-05-17 12:08:02 +0900

    update example/app.js for PR#2

--- example/app.js
+++ example/app.js
@@ -3,16 +3,83 @@
 // to test out the module and to provide instructions 
 // to users on how to use it by example.
 
+var correlationId = 'TiTestFairyExampleId';
+var checkpointName = 'ExampleCheckPoint';
 
 // open a single window
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+  backgroundColor:'white',
+  layout: 'vertical'
 });
-var label = Ti.UI.createLabel();
+
+var label = Ti.UI.createLabel({
+  top: 50,
+  text: 'TiTestFairy',
+  textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
+});
 win.add(label);
-win.open();
+
+var cidLabel = Ti.UI.createLabel({
+  text: '(Correlation ID: ' + correlationId + ')',
+  textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+  font: { fontSize: 12 }
+});
+win.add(cidLabel);
+
+var pauseButton = Ti.UI.createButton({ title: 'Pause', width: 80, top: 30 });
+win.add(pauseButton);
+
+var screenshotButton = Ti.UI.createButton({ title: 'Take Screenshot', top: 30 });
+win.add(screenshotButton);
+
+var sessionUrlButton = Ti.UI.createButton({ title: 'Get Session URL', top: 30 });
+win.add(sessionUrlButton);
+
+var checkpointButton = Ti.UI.createButton({ title: 'Create Checkpoint', top: 30 });
+win.add(checkpointButton);
+var checkpointLabel = Ti.UI.createLabel({
+  text: '(Checkpoint Name: ' + checkpointName + ')',
+  textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+  font: { fontSize: 12 }
+});
+win.add(checkpointLabel);
+
+var feedbackButton = Ti.UI.createButton({ title: 'Feedback', top: 30 });
+win.add(feedbackButton);
 
 var TiTestFairy = require('com.arihiro.titestfairy');
 Ti.API.info("module is => " + TiTestFairy);
 
-TiTestFairy.begin("thisistesttoken");
+// TiTestFairy.begin("thisistesttoken");
+TiTestFairy.begin("0cc214fceff316e8eccd3882115ecb63d5d166b7");
+TiTestFairy.setCorrelationId(correlationId);
+
+var paused = false
+pauseButton.addEventListener('click', function() {
+  if (paused) {
+    TiTestFairy.resume();
+    pauseButton.title = 'Pause';
+  } else {
+    TiTestFairy.pause();
+    pauseButton.title = 'Resume';
+  }
+  paused = !paused;
+});
+
+screenshotButton.addEventListener('click', function() {
+  TiTestFairy.takeScreenshot();
+});
+
+sessionUrlButton.addEventListener('click', function() {
+  alert(TiTestFairy.sessionUrl());
+});
+
+checkpointButton.addEventListener('click', function() {
+  TiTestFairy.checkpoint(checkpointName);
+});
+
+feedbackButton.addEventListener('click', function() {
+  TiTestFairy.pushFeedbackController();
+});
+
+win.open();
